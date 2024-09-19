122d4cb5f13e34ac9fcb95b9fe2c7d0c8af5c4c4
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-09-19 19:07:27 +0530

    Add the button and button functionality

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-for (let i = 0; i < 16; i++) {
-  let container = document.createElement("div");
-  let body = document.querySelector("body");
-  body.appendChild(container);
-  console.log("i " + i);
-  for (let j = 0; j < 16; j++) {
-    let box = document.createElement("div");
-    container.appendChild(box);
-    box.addEventListener("mouseover", () => {
-      box.classList.add("hover-color");
-    });
+function createGrid(n) {
+  let containers = document.querySelector(".containers");
+  while (containers.hasChildNodes()) {
+    containers.removeChild(containers.firstChild);
   }
-  container.classList.toggle("horizontal-grid");
+  for (let i = 0; i < n; i++) {
+    let container = document.createElement("div");
+    containers.appendChild(container);
+    console.log("i " + i);
+    for (let j = 0; j < n; j++) {
+      let box = document.createElement("div");
+      container.appendChild(box);
+      box.addEventListener("mouseover", () => {
+        box.classList.add("hover-color");
+      });
+    }
+    container.classList.toggle("horizontal-grid");
+  }
 }
+createGrid(16);
+const popUpButton = document.querySelector("button");
+popUpButton.addEventListener("click", () => {
+  let n = prompt("Enter n (n < 100) :");
+  createGrid(n);
+});
