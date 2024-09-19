88808b0c5884acff28a2939f94acba343da785c7
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-09-19 15:45:23 +0530

    Create a 16*16 grid
    This commit creates a 16*16 grid using DOM and javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 16; i++) {
+  let container = document.createElement("div");
+  let body = document.querySelector("body");
+  body.appendChild(container);
+  console.log("i " + i);
+  for (let j = 0; j < 16; j++) {
+    let box = document.createElement("div");
+    container.appendChild(box);
+  }
+  container.classList.toggle("horizontal-grid");
+}
